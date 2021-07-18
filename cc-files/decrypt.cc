e9ebb66932ac5fec738b7da13643cb7fdaea7345
Daniel Rath <83286956+danielra01@users.noreply.github.com>
2021-07-18 18:04:00 +0200

    Added some logic for decryption

--- cc-files/decrypt.cc
+++ cc-files/decrypt.cc
@@ -4,11 +4,29 @@
 
 #include <iostream>
 #include <string>
+#include <queue>
+#include <bits/stdc++.h>
 
 int main() {
 
+    std::string encrypted_text = "Hallo das ist ein Test!";    
+    // Check for new e
+    // Create Queue
+    priority_queue<pair<int, char>> char_queue;
 
- //TODO: Add code for input outout and decryption logic
+    for (int i = 1; i <= 26; i++){
+        curent_char = (char)(96 + i);
+        counter = 0;
+        for (int j = 0; j < encrypted_text.size(); j++){
+            if (encrypted_text[j] == current_char) {
+                counter++;
+            }
+        }
 
+        char_queue.push(make_pair(counter, current_char);
+    }
+
+
+    return 1;
 }
 
